3c3e23d0dbebb5dcfba0d8410885699131cc9c5f
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-05-05 00:55:41 +0600

    Update Sliding Range Maximum Query.java

--- Algorithm/src/Sliding Range Maximum Query.java
+++ Algorithm/src/Sliding Range Maximum Query.java
@@ -34,3 +34,34 @@ public class SlidingRMQ {
         letsTry(n,k,arr);
     }
 }
+
+//Sliding Window Maximum Range Easy
+import java.util.*;
+
+public class SlidingWindowMax {
+    static void SWM(int[] arr,int n,int k){
+        Deque<Integer>Q=new ArrayDeque<>();
+        for (int i=0;i<n;i++){
+            if(!Q.isEmpty() && Q.peek()==i-k){
+                Q.pollFirst();
+            }
+            while (!Q.isEmpty() && arr[i]>arr[Q.peekLast()]){
+                Q.pollLast();
+            }
+            Q.offerLast(i);
+            if(i>=k-1){
+                System.out.println(arr[Q.peek()]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),k=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        SWM(arr,n,k);
+    }
+}
+
